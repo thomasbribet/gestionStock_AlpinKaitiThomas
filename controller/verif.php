e0602ccd6b0cf5cfd923529e9f787cc6ef89fbb6
Thomas <thomas.bribet@gmail.com>
2021-06-15 10:48:23 +0200

    plus de commentaires

--- controller/verif.php
+++ controller/verif.php
@@ -9,9 +9,8 @@ function userVerif()
     // On fait une requête à la bdd, et on stocke le résultat dans $result (sous forme de tableau)
     $result = $conn->query('SELECT * FROM users WHERE email="' . $_POST["username"] . '" AND password="' . $_POST["inputpassword"] . '";');
 
-    // On parcours le tableau $result grâce à fetch_row(), et on le stocke dans $row
+    // On parcours le tableau $result grâce à fetch_row(), et on le stocke ce résultat dans $row (qui est un tableau lui aussi)
     $row = $result->fetch_row();
-    var_dump($row);
 
     // On vérifie si le tableau $row est vide ( grâce à empty() ) => on vérifie donc si les données (entrées dans le formulaire de connexion) sont présentes dans la bdd
     if (empty($row)) {
