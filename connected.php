61edb550870bbc889941ee4d70175d6ae55191ab
Thomas <thomas.bribet@gmail.com>
2021-06-15 14:04:55 +0200

    accés à la page connected.php après authentification

---
+++ connected.php
@@ -0,0 +1,4 @@
+<?php 
+
+ require_once("./views/header.php");
+ require_once("./views/footer.php");
--- controller/verif.php
+++ controller/verif.php
@@ -6,19 +6,26 @@ function userVerif()
 {
     global $conn;
 
-    // On fait une requête à la bdd, et on stocke le résultat dans $result (sous forme de tableau)
-    $result = $conn->query('SELECT * FROM users WHERE email="' . $_POST["username"] . '" AND password="' . $_POST["inputpassword"] . '";');
+    if (isset($_POST["username"]) && isset($_POST["inputpassword"])) {
 
-    // On parcours le tableau $result grâce à fetch_row(), et on le stocke ce résultat dans $row (qui est un tableau lui aussi)
-    $row = $result->fetch_row();
+        // On fait une requête à la bdd, et on stocke le résultat dans $result (sous forme de tableau)
+        $result = $conn->query('SELECT * FROM users WHERE email="' . $_POST["username"] . '" AND password="' . $_POST["inputpassword"] . '";');
 
-    // On vérifie si le tableau $row est vide ( grâce à empty() ) => on vérifie donc si les données (entrées dans le formulaire de connexion) sont présentes dans la bdd
-    if (empty($row)) {
-        echo "pas ok";
-        // si $row est vide, on redirige vers la page d'accueil
-        header("Location: ../index.php");
-    } else {
-        echo "ok";
+        // On parcours le tableau $result grâce à fetch_row(), et on le stocke ce résultat dans $row (qui est un tableau lui aussi)
+        $row = $result->fetch_row();
+
+        // On vérifie si le tableau $row est vide ( grâce à empty() ) => on vérifie donc si les données (entrées dans le formulaire de connexion) sont présentes dans la bdd
+        if (empty($row)) {
+            echo "pas ok";
+            // si $row est vide, on redirige vers la page d'accueil
+            header("Location: ../index.php");
+        } else {
+            echo "ok";
+            session_start();
+            $_SESSION["username"] = $_POST["username"];
+            $_SESSION["inputpassword"] = $_POST["inputpassword"];
+            header("Location: ../connected.php");
+        }
     }
 }
 
