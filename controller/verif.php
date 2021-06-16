dc4f981cd0edf0bad2102da6852bf12fb3a710fa
Thomas <thomas.bribet@gmail.com>
2021-06-16 14:56:45 +0200

    icones faits dans page connected

--- controller/verif.php
+++ controller/verif.php
@@ -11,7 +11,6 @@ function userVerif()
         // On fait une requête à la bdd, et on stocke le résultat dans $result (sous forme de tableau)
         $result = $conn->query('SELECT * FROM users WHERE email="' . $_POST["username"] . '" AND password="' . hash('sha256', $_POST["inputpassword"]) . '";');
 
-        $prenom_db = $conn->query('SELECT prenom FROM users WHERE email="' . $_POST["username"] . '" AND password="' . hash('sha256', $_POST["inputpassword"]) . '";');
         // On parcours le tableau $result grâce à fetch_row(), et on le stocke ce résultat dans $row (qui est un tableau lui aussi)
         $row = $result->fetch_row();
 
@@ -25,7 +24,6 @@ function userVerif()
             $_SESSION["username"] = $_POST["username"];
             $_SESSION["inputpassword"] = $_POST["inputpassword"];
             
-
             header("Location: ../connected.php");
         }
     }
--- views/header.php
+++ views/header.php
@@ -18,16 +18,15 @@ session_start();
 <body>
     <header class="col-12 d-flex justify-content-between align-items-center bg-secondary text-white px-3" style="height: 10vh;">
         <a style="height: 100%;" class="d-flex align-items-center" href="./index.php"><img src="./assets/logo" alt="" style="height: 80%; width: auto; border-radius: 5px;"></a>
-        <p class="m-0 p-0 fs-4">Bonjour à <?php if (!isset($_SESSION["username"])) {
-                                                echo "vous";
-                                            } else {
-                                                echo $_SESSION["username"];
-                                            }
-                                            ?> !
+        <p class="m-0 p-0 fs-4">Bonjour<?php if (!isset($_SESSION["username"])) {
+                                            echo " à vous";
+                                        } else {
+                                            echo " " . $_SESSION["username"];
+                                        }
+                                        ?> !
         </p>
         <?php if (isset($_SESSION["username"])) {
             echo '<a class="text-white" href="./controller/deconnexion.php">Déconnexion</a>';
         }
         ?>
-
     </header>
